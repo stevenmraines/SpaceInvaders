7fa4bdb279d354d7fc2029afd1b779e8c782e8b7
imjonsnow <dwo6030@uncw.edu>
2014-11-09 09:35:56 -0500

    Update Bunker.java

--- Bunker.java
+++ Bunker.java
@@ -1,8 +1,13 @@
 import java.awt.Graphics;
 //Bunkers are not moving objects and thus extends ScreenObject.
+import java.awt.Point;
+import java.awt.Rectangle;
 
 public class Bunker extends ScreenObject {
 
+	public Bunker(Point location, Rectangle size){
+		super(location, size);
+	}
 	@Override
 	public void draw(Graphics g) {
 		// TODO Auto-generated method stub
@@ -10,3 +15,4 @@ public class Bunker extends ScreenObject {
 	}
 
 }
+
