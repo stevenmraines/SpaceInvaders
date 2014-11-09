23dcd43afdbe73afe44fa760181e226948c14fb3
imjonsnow <dwo6030@uncw.edu>
2014-11-09 09:37:35 -0500

    Update Invader.java

--- Invader.java
+++ Invader.java
@@ -1,4 +1,11 @@
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.Rectangle;
+
 
 public class Invader extends Enemy {
 //Need more classes for types of invaders?
+	public Invader(Point p, Rectangle s, int points, Image i){
+		super(p, s, points, i);
+	}
 }
