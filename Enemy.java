d35c8ee396cb86fa8d4b8f797f9adace41642009
imjonsnow <dwo6030@uncw.edu>
2014-11-09 09:36:39 -0500

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -1,4 +1,34 @@
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.Rectangle;
+
 //Enemies contain points to add to score upon destruction
 public class Enemy extends MovingScreenObject {
+	/**
+	 * @author Derick Owens
+	 */
+	protected int pointValue;
+	
+	/**
+	 * 
+	 */
+	public Enemy(Point p, Rectangle s, int points, Image i){
+		super(p, s, i, 0);
+		pointValue = points;
+		
+	}
 
+	/**
+	 * @return the pointValue
+	 */
+	public int getPointValue() {
+		return pointValue;
+	}
+
+	/**
+	 * @param pointValue the pointValue to set
+	 */
+	public void setPointValue(int pointValue) {
+		this.pointValue = pointValue;
+	}
 }
