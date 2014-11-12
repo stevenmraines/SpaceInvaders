03b31af5ec185c5bb70291b8e324d7c072449351
imjonsnow <dwo6030@uncw.edu>
2014-11-12 15:09:41 -0500

    Update Invader.java
    Added overrides to MovingScreenObjects move() method.
    Makes all invaders oscillate rather than appear in -x when they approach the edge of the screen.

--- Invader.java
+++ Invader.java
@@ -4,8 +4,34 @@ import java.awt.Rectangle;
 
 
 public class Invader extends Enemy {
-//Need more classes for types of invaders?
+	public static int baseY = 500;
 	public Invader(Point p, Rectangle s, int points, Image i){
 		super(p, s, points, i);
+	}
+	public void move() 
+	{
+		location.x += arbitraryVector.getChangeX();
+		location.y += arbitraryVector.getChangeY();
+		
+		if (location.x == Screen.screenWidth-100) {
+			arbitraryVector.setChangeX(-1);
+			this.setBaseY(baseY - 20); 
+		}
+		if (location.x == 100) {
+			arbitraryVector.setChangeX(1);
+			this.setBaseY(baseY - 20);
+		}
+	}
+	/**
+	 * @return the baseY
+	 */
+	public static int getBaseY() {
+		return baseY;
+	}
+	/**
+	 * @param baseY the baseY to set
+	 */
+	public void setBaseY(int baseY) {
+		this.baseY = baseY;
 	}
 }
