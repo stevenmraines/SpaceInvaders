a17fb7ae3621280b879e0d9b091ccaca29396831
imjonsnow <dwo6030@uncw.edu>
2014-11-21 09:58:55 -0500

    Added Getter/Setter for invaderPoints variable

--- Invader.java
+++ Invader.java
@@ -6,8 +6,10 @@ import java.awt.Rectangle;
 public class Invader extends Enemy {
 	public static int baseY = 325;
 	public static ObjectVector invaderVector = new ObjectVector(1,0);
+	public static int invaderPoints;
 	public Invader(Point p, Rectangle s, int points, Image i){
 		super(p, s, points, i);
+		invaderPoints = points;
 	}
 	public void move() 
 	{
@@ -37,5 +39,17 @@ public class Invader extends Enemy {
 	 */
 	public static void setInvaderVector(ObjectVector invaderVector) {
 		Invader.invaderVector = invaderVector;
+	}
+	/**
+	 * @return the invaderPoints
+	 */
+	public static int getInvaderPoints() {
+		return invaderPoints;
+	}
+	/**
+	 * @param invaderPoints the invaderPoints to set
+	 */
+	public static void setInvaderPoints(int invaderPoints) {
+		Invader.invaderPoints = invaderPoints;
 	}
 }
