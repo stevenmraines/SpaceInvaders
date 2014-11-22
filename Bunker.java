53790bb2b4150e0ad9af8703a6c1254898ef52d7
stevenmraines <smr3220@uncw.edu>
2014-11-22 08:46:26 -0500

    Update Bunker.java
    Added hit points to the bunker that will be decremented by 1 every time it's hit by an alien projectile

--- Bunker.java
+++ Bunker.java
@@ -5,9 +5,17 @@ import java.awt.Point;
 import java.awt.Rectangle;
 
 public class Bunker extends MovingScreenObject {
+	private int hitPoints = 40;
+	
 	public Bunker(Point location, Rectangle size, Image i){
 		super(location, size, i, 0);
 	}
+	
+	public int getHitPoints() {
+		return hitPoints;
 	}
-
-
+	
+	public void setHitPoints(int hp) {
+		hitPoints = hp;
+	}
+}
