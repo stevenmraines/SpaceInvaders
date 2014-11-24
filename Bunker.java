54d88e7a902be6f86c7e37e7914777f7a73fd730
imjonsnow <dwo6030@uncw.edu>
2014-11-24 18:39:33 -0500

    javadocs

--- Bunker.java
+++ Bunker.java
@@ -4,17 +4,36 @@ import java.awt.Image;
 import java.awt.Point;
 import java.awt.Rectangle;
 
+/**
+ * 
+ * @author Derick Owens
+ *
+ */
 public class Bunker extends MovingScreenObject {
 	private int hitPoints = 40;
 	
+	/**
+	 * 
+	 * @param location location of the Bunker
+	 * @param size size of the Bunker
+	 * @param i image of the Bunker
+	 */
 	public Bunker(Point location, Rectangle size, Image i){
 		super(location, size, i, 0);
 	}
 	
+	/**
+	 * 
+	 * @return hitPoints, which are reduced when a bunker is struck by a shot
+	 */
 	public int getHitPoints() {
 		return hitPoints;
 	}
 	
+	/**
+	 * 
+	 * @param hp set the hit points
+	 */
 	public void setHitPoints(int hp) {
 		hitPoints = hp;
 	}
