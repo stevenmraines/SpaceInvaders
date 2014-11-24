64cba103498845cdcfbb0b031b3e42b57cdb90fe
imjonsnow <dwo6030@uncw.edu>
2014-11-24 18:37:23 -0500

    javadocs

--- Invader.java
+++ Invader.java
@@ -1,16 +1,32 @@
 import java.awt.Image;
 import java.awt.Point;
 import java.awt.Rectangle;
-
+/**
+ * 
+ * @author Derick Owens
+ *
+ */
 
 public class Invader extends Enemy {
 	public static int baseY = 325;
 	public static ObjectVector invaderVector = new ObjectVector(1,0);
 	public int invaderPoints;
+	
+	/**
+	 * 
+	 * @param p location of the invader
+	 * @param s size of the invader
+	 * @param points value of the invader
+	 * @param i image of the invader
+	 */
 	public Invader(Point p, Rectangle s, int points, Image i){
 		super(p, s, points, i);
 		invaderPoints = points;
 	}
+	/**
+	 * Move the invader
+	 */
+	@Override
 	public void move() 
 	{
 		location.x += invaderVector.getChangeX();
