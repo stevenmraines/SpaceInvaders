9b67996a44dbc4c5395a6036e1d7708b8f7df8e0
stevenmraines <smr3220@uncw.edu>
2014-11-25 20:25:56 -0500

    Update GameWindow.java

--- GameWindow.java
+++ GameWindow.java
@@ -1,4 +1,6 @@
+import java.util.concurrent.TimeUnit;
 import javax.swing.JFrame;
+
 /**
  * @author Derick Owens
  *
@@ -21,7 +23,23 @@ public class GameWindow extends JFrame {
 	 * @param args Ignored.
 	 */
 	public static void main(String[] args){
-		GameWindow window = new GameWindow();
+		JFrame startFrame = new JFrame("Space Invaders");
+		StartScreen start = new StartScreen();
+		startFrame.add(start);
+		startFrame.pack();
+		startFrame.setResizable(false);
+		startFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		startFrame.setVisible(true);
+		
+		try {
+			TimeUnit.SECONDS.sleep(4);
+		}
+		catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		startFrame.dispose();
+		
+		new GameWindow();
 	}
 }
-
